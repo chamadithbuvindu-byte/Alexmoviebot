eefc7aa8b88f77e17d33eb5ca858eaabdb4494d0
chamadithbuvindu-byte <chamadithbuvindu@gmail.com>
2026-04-25 20:17:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-API_ID = 1234567
-API_HASH = "YOUR_API_HASH"
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+API_ID = 25733173
+API_HASH = "fea626c63bf648e1d8d11b35ff5b5570"
+BOT_TOKEN = "8746836615:AAEHfJscJ9gluedmg7JbWdGbZ6wpKL4LnRQ"
 
-MONGO_URL = "YOUR_MONGO_URL"
+MONGO_URL = "mongodb+srv://chamadithbuvindu_db_user:6kQaqQ0XSFPax9SO@cluster0.ywevrcf.mongodb.net/?appName=Cluster0"
 
-CHANNEL_ID = -1001234567890
-CHANNEL_USERNAME = "your_channel_username"
+CHANNEL_ID = -1003994779583
+CHANNEL_USERNAME = "https://t.me/SHADOW_movie23"
 
-ADMIN_IDS = [123456789]
+ADMIN_IDS = [8458894963]
 
-OMDB_API = "YOUR_OMDB_API_KEY"
+OMDB_API = "http://www.omdbapi.com/?i=tt3896198&apikey=43bad0e0"
